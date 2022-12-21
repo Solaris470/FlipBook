5e22d109021b201c01fe9328ac34537503ae7abd
Fluk <64309010013@bncc.ac.th>
2022-12-21 09:05:06 +0700

    front

---
+++ resources/views/front/ebook.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{url('/css/style.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css">
+    <title>Flipbook</title>
+</head>
+<body>
+    <div id="test">
+        <div class='tools'>
+            <i id='qrcode' class="fa-solid fa-qrcode" style="font-size: 40px;"></i>
+            <a href="{{url('/')}}"><i id='close' class="fa-regular fa-circle-xmark" style="font-size: 40px;"></i></a>
+        </div>
+        <div id="opqrcode">
+            <div>
+                {!! QrCode::size(300)->generate(url('/flipbook/'.$val)) !!}
+            </div>
+        </div>
+        <div class="test">
+            <div id="book">
+                <div style="background:url('{{url('/img/'.$val.'/01.jpg')}}'); background-size:100% 100%;"></div>
+                <div style="background:url('{{url('/img/'.$val.'/02.jpg')}}'); background-size:cover;"></div>
+                <div style="background:url('{{url('/img/'.$val.'/03.jpg')}}'); background-size:cover;"></div>
+                <div style="background:url('{{url('/img/'.$val.'/04.jpg')}}'); background-size:cover;"></div>
+                <div style="background:url('{{url('/img/'.$val.'/05.jpg')}}'); background-size:cover;"></div>
+                <div style="background:url('{{url('/img/'.$val.'/06.jpg')}}'); background-size:cover;"></div>
+            </div>
+        </div>
+    </div>
+    <script src="https://kit.fontawesome.com/97a4ddb345.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.min.js"></script>
+    <script src="{{url('/js/turn.js')}}"></script>
+    <script>
+        $(document).ready(function(){
+            // $('.open').click(function(){
+            //     $('#test').css('display','block');
+            //     $('#con').css('display','none');
+            // });
+            // $('#close').click(function(){
+                // $('#test').css('display','none');
+                // $('#con').css('display','flex');
+            // });
+            $('#qrcode').click(function(){
+                $('#opqrcode').css('display','flex');
+                $('#book').css('display','none');
+                $('.tools').css('display','none');
+            });
+        });
+        $('#book').turn({gradient:true,acceleration:true});
+    </script>
+</body>
+</html>
